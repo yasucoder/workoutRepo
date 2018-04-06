eb6b427351dcd59a705a5a3b676f42ab522556ff
yasucoder <suyatchi@gmail.com>
2018-04-06 10:25:11 -0700

    Spring Security Samples

---
+++ Spring/SpringSecurity/SpringSecurityApp/src/main/java/com/garage/yasu/SpringSecurity/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.garage.yasu.SpringSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.garage.yasu.SpringSecurity")
+public class AppConfig {
+	
+	//define a bean for ViewResolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
---
+++ Spring/SpringSecurity/SpringSecurityApp/src/main/java/com/garage/yasu/SpringSecurity/config/SecurityWebApplicationIntializer.java
@@ -0,0 +1,8 @@
+package com.garage.yasu.SpringSecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationIntializer extends
+		AbstractSecurityWebApplicationInitializer {
+
+}
